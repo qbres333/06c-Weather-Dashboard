0d677ee2235734dc0f623191510214331afa67e8
Fin M <qbres333@hotmail.com>
2024-08-19 17:40:07 -0600

    add weather object data, without conversions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,7 +4,8 @@ const cityHeaderEl = document.getElementByID("city-header");
 const apiKey = '32545b0997265663df9ff6fd8f6c9fca'
 
 
-//get latitude and longitude data by searching the city name
+/* get latitude and longitude data by searching the city name,
+then return today's weather */ 
 function fetchGeocodeData(city) {
     // URL for direct Geocoding API call - converts city name input to coordinates
     const geocodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=${apiKey}`;
@@ -73,47 +74,64 @@ function getStoredData() {
     return cityData;
 }
 
+// retrieve 5-day forecast
 function fetchForecast(city) {
-  
-  //   const city = cityData
-  if(cityData["cityName"] != city){
-    // error checking done in 
-    console.error("City data not found for:", city);
-  } else {
+    // parse stored data (if any)
     fetchGeocodeData(city);
-    // get lat and lon data from the fetchGeocodeData function
     const cityData = getStoredData();
+    
+    if(cityData.cityName != city){
+        /* error checking done in fetchGeocode function; shouldn't be 
+        needed here but will print to console */
+        console.error("City data not found for:", city);
+    } else {        
+        // get lat/lon data from the parsed data        
+        const lat = cityData.cityLatitude;
+        const lon = cityData.cityLongitude;
+        // URL for fetching weather data by latitude,longitude
+        const regURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-    const lat = cityData["cityName"].cityLatitude;
-    const lon = cityData["cityName"].cityLongitude;
-    // URL for fetching weather data by latitude,longitude
-    const regURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-    //send API request
-    fetch(regURL)
-      .then(function (response) {
-        // if the request fails, show the JSON error. Otherwise return the promise.
-        if (!response.ok) {
-          throw response.json();
-        }
-        return response.json();
-      })
-      .then(function (weatherData) {
-        // create an array to store the various weather elements
-        const weatherArray = [];
-
-        // search API for city weather info
-        for (const cityWeather of weatherData) {
-          if (typeof cityWeather === "object") {
-            if ((cityWeather[0].name = city)) {
+        //send API request
+        fetch(regURL)
+        .then(function (response) {
+            // if the request fails, show the JSON error. Otherwise return the promise.
+            if (!response.ok) {
+                throw response.json();
             }
-          }
-        }
+            return response.json();
+        })
+        .then(function (weatherData) {
+            /* create an array to store the various weather elements (date, icon, temp, wind, humidity) */
+            const weatherArray = [];
 
-        // checks for entries with the city's name
-      });
-  }
-  
+            // retrieve city weather info
+            for (const cityWeather of weatherData) {                
+                if (typeof cityWeather === "object") {
+                    const weatherObj = {};
+                    weatherObj["name"] = cityWeather.city.name;
+                    // Weather is in 3hr increments for each day. Return the 12pm temp data (5th element)
+                    //API date is a unix timestamp. Convert.
+                    const dateToday = dayjs(cityWeather.list[5].dt, "MM/DD/YYYY");
+                    weatherObj["date"] = dateToday;
+                    // weather icon
+                    weatherObj["icon"] = cityWeather.list[5].weather.icon;
+                    // API temperature is in Kelvins. Call function to convert to F
+                    weatherObj["temp"] = cityWeather.list[5].main.temp;
+                    // API wind speed is in meters/sec. Call function to convert to MPH
+                    weatherObj["wind"] = cityWeather.list[5].wind.speed;
+                    // API humidity is a percentage
+                    weatherObj["humidity"] = cityWeather.list[5].main.humidity;
+            
+                }
+
+
+
+            }
+
+            // checks for entries with the city's name
+        });
+    }
+
 
 
 
